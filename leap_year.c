a82e6a7c3fd53c8d388c80cbe54765de44184968
mradul kumar katiyar <dev.mradul@proton.me>
2025-05-10 09:22:10 +0530

    leap year

---
+++ leap_year.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(){
+  int year = 0;
+
+  printf("Enter the number: ");
+  scanf("%d", &year);
+
+  if (year % 4 == 0){
+    printf("%d is a leap year", year);
+  }
+  else{
+    printf("%d is not a leap year", year);
+  }
+}

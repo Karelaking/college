84ce7dca05daf92c3579757f34622d40ae885736
mradul kumar katiyar <dev.mradul@proton.me>
2025-05-10 09:19:51 +0530

    factorial of the number

--- factorial.c
+++ factorial.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
+// Function to calculate the factorial of the given number
 int factorial(int number){
   int result = 1;
-  for (int i = number; i == 1; i--){
+  for (int i = number; i >= 1; i--){
     result = result * i;
   }
+  return result;
 }
 
 void main(){
-  int fac = factorial(5);
-  printf("%d", fac);
+  int number;
+
+  printf("Enter the number: ");
+  scanf("%d", &number);
+
+  int factorial = factorial(number);
+
+  printf(
+
+    
+  )
 }
 
 // 5*4*3*2*1 = 120
